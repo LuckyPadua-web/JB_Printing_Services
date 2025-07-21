3bec7439ea6a7466327c8c9c585d04d5199b8e8c
GLENN <glenard0823@gmail.com>
2025-07-21 09:01:52 +0800

    `Update contact information in footer: email, opening hours, address, map link, and phone number.`

--- components/footer.php
+++ components/footer.php
@@ -5,26 +5,26 @@
       <div class="box">
          <img src="images/email-icon.png" alt="" >
          <h3>Our Gmail address</h3>
-         <a href="">---</a>
+         <a href="">jbbaui15@gmail.com</a>
           
       </div>
 
       <div class="box">
          <img src="images/clock-icon.png" alt="">
          <h3>Opening hours</h3>
-         <p>---</p>
+         <p>7:00 am to 5:00 pm</p>
       </div>
 
       <div class="box">
          <img src="images/map-icon.png" alt="">
          <h3>Our Address</h3>
-         <a href="https://goo.gl/maps/ccZWZwcpdRZX5ZSj7">---</a>
+         <a href="https://goo.gl/maps/ccZWZwcpdRZX5ZSj7">Poblacion 1, Piat, Cagayan</a>
       </div>
 
       <div class="box">
          <img src="images/phone-icon.png" alt="">
          <h3>our number</h3>
-         <a href="#">---</a>
+         <a href="#">0935 971 8567</a>
          
       </div>
 
